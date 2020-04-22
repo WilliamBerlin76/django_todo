b3ae7d32dd2d13581f3ab8bdb6de45decd7a058c
WilliamBerlin76 <william.berlin76@gmail.com>
2020-04-21 20:04:27 -0500

    added models for Todos and userTodo

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 from uuid import uuid4
 
+from accounts.models import Account
+
+class Todo(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    priority = models.IntegerField(blank=True)
+
+class UserTodo(Todo):
+    user = models.ForeignKey(Account, on_delete=models.CASCADE)
