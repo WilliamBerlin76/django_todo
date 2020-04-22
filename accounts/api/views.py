808cc1df8f3586d2890f535665cbec1ca759c3b6
WilliamBerlin76 <william.berlin76@gmail.com>
2020-04-22 13:03:52 -0500

    registering new users now generates and returns auth token

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -1,9 +1,10 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-
+from rest_framework.authtoken import views
 
 from accounts.api.serializers import RegistrationSerializer
+from rest_framework.authtoken.models import Token
 
 @api_view(['POST'])
 def registration_view(request):
@@ -16,6 +17,8 @@ def registration_view(request):
             data['response'] = 'successfully registered'
             data['email'] = account.email
             data['username'] = account.username
+            token = Token.objects.get(user=account).key
+            data['token'] = token
         else:
             data = serializer.errors
         
